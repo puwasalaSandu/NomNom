752d7817f0384682cfe0be367fc6368be51b4be2
madproject123 <projectmad07@gmail.com>
2019-07-30 22:28:47 +0530

    Commit Three

--- app/src/main/java/com/example/myapplication/Login.java
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -2,13 +2,33 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+
+    private EditText Username;
+    private EditText Password;
+    private Button Login;
+    private Button Register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Username = (EditText)findViewById(R.id.user);
+        Password = (EditText)findViewById(R.id.pass);
+        Login = (Button)findViewById(R.id.btn2);
+        Register = (Button)findViewById(R.id.btn1);
+    }
+
+    private void validate(String userName,String Password){
+        if((userName == "Admin") && (Password == "1234")){
+            Intent intent = new Intent(Login.this,Menu.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT =4000;
@@ -14,13 +16,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent homeIntent = new Intent(MainActivity.this, Login.class);
                 startActivity(homeIntent);
                 finish();
-
             }
         },SPLASH_TIME_OUT);
     }
